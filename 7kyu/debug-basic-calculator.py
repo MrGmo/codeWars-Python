91110eb3c1a5f0fe37ef332cc4731c62f478d96c
Guillermo <gaguilar128@gmail.com>
2021-12-02 20:38:24 -0600

    Codewars practice

---
+++ 7kyu/debug-basic-calculator.py
@@ -0,0 +1,11 @@
+def calculate(a, o, b):
+    result = 0
+    if o == "+":
+        return a+b
+    if o == "-":
+        return a-b
+    if o == "/" and b != 0:
+        return a/b
+    if o == "*":
+        return a*b
+    return None
